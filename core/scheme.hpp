12ce4e4cc0776bcb58b92b3355dcd670b5809c29
giorgi <daphne.giorgi@gmail.com>
2014-11-26 13:49:15 +0100

    add phischeme to scheme

--- core/scheme.hpp
+++ core/scheme.hpp
@@ -190,118 +190,6 @@ pair<StateType, StateType> Euler<StateType, VolType, RandomType>::doubleSimulati
     return pairProcess;
 }
 
-///**
-// * @param model Pointer to the model
-// * @param scheme Pointer to the scheme
-// */
-//PhiScheme::PhiScheme(const modelPtr model, const schemePtr scheme, std::function<double(double)> f): Scheme(model), m_scheme(scheme), m_f(f){}
-
-///**
-// * @brief Method to make a transition of step h, at time t, starting from x and with a random realization random.
-// *
-// * @param t Starting time.
-// * @param x Starting point.
-// * @param h Time step.
-// * @param r Random realization.
-// */
-//double PhiScheme::transition(double t, double x, double h, double r)
-//{
-////    return m_model->drift(t,x)*h + m_model->sigma(t, x)*sqrt(h)*m_model->random(gen);
-//    return m_model->drift(t,x)*h + m_model->sigma(t, x)*sqrt(h)*r;
-//}
-
-///**
-// * @brief Method to make a transition for two different discretizations of the same process, over step h, one with discretization 1 and one with discretization n,
-// * starting from x at time t.
-// *
-// * @param gen Random generator.
-// * @param t Starting time.
-// * @param x Starting point.
-// * @param h Time step.
-// * @param n Discretization for the finest process.
-// */
-//pair<double, double> PhiScheme::pairTransition(mt19937_64 &gen, double t, pair<double, double> pairProcess, double h, unsigned int n)
-//{
-//    // We get the finest step
-//    double h_n = h/n;
-
-//    // We get the starting points
-//    double X_0 = pairProcess.first;
-//    double X_n = pairProcess.second;
-
-//    double randomRealization = 0;
-//    double randomRealizationSum = 0;
-
-//    // We make n transitions on the finest process, keeping memory of the random realizations
-//    for (unsigned int i=0; i<n; ++i){
-//        randomRealization = m_model->random(gen)*sqrt(h_n);
-//        X_n = transition(t + i*h_n, X_n, h, randomRealization);
-//        randomRealizationSum += randomRealization;
-//    }
-
-//    // We make one transition on the biggest process
-//    X_0 = transition(t, X_0, h, randomRealizationSum);
-
-//    return pair<double, double>(X_0, X_n);
-//}
-
-
-///**
-// * @brief PhiScheme::singleSimulation
-// * @param gen
-// * @param n
-// * @return
-// */
-//double PhiScheme::singleSimulation(mt19937_64 &gen, const unsigned int n)
-//{
-////    double currentTime = 0.;
-
-////    double X_i = m_model->getStartingPoint();
-////    double h = m_model->getMaturity()/(double)n;
-
-////    for (unsigned int i=0; i<n; i++){
-////        X_i += m_model->drift(currentTime,X_i)*h + m_model->sigma(currentTime, X_i)*sqrt(h)*m_model->random(gen);
-////        currentTime += h;
-////    }
-
-////    return X_i;
-//    return 0;
-//}
-
-//pair<double, double> PhiScheme::doubleSimulation(mt19937_64 &gen, const unsigned int n1, const unsigned int n2)
-//{
-//    return pair<double,double>(0,0);
-//}
-
-
-
-
-//class PhiScheme
-//{
-//public:
-//    /** Constructor. */
-//    PhiScheme(const modelPtr model, const schemePtr scheme, std::function<Eigen::VectorXd(Eigen::VectorXd)> phi);
-
-//    pair<Eigen::VectorXd,Eigen::VectorXd>  transition(double t, pair<Eigen::VectorXd,Eigen::VectorXd> x, double h, Eigen::VectorXd random);
-//    pair<pair<Eigen::VectorXd,Eigen::VectorXd>,pair<Eigen::VectorXd,Eigen::VectorXd>> pairTransition(mt19937_64& gen, double t, pair<pair<Eigen::VectorXd,Eigen::VectorXd>,pair<Eigen::VectorXd,Eigen::VectorXd>>  x, double h, unsigned int n);
-
-//    pair<Eigen::VectorXd, Eigen::VectorXd> singleSimulation(mt19937_64& gen, const unsigned int n);
-//    pair<pair<Eigen::VectorXd,Eigen::VectorXd>,pair<Eigen::VectorXd,Eigen::VectorXd>> doubleSimulation(mt19937_64& gen, const unsigned int n1, const unsigned int n2);
-
-//protected:
-//    modelPtr m_model;
-//    schemePtr m_scheme;
-//    std::function<double(double)> m_phi;
-//};
-
-// Smart pointers to PhiScheme objects
-typedef shared_ptr<PhiScheme> phiSchemePtr;
-
-//template<typename state> void saySomething(state thisstate)
-//{
-//    cout << thisstate << endl;
-//}
-
 // Smart pointer to Scheme objects
 template<typename StateType, typename VolType, typename RandomType>
 using schemePtr = shared_ptr<Scheme<StateType, VolType, RandomType>> ;
@@ -309,5 +197,183 @@ using schemePtr = shared_ptr<Scheme<StateType, VolType, RandomType>> ;
 // Smart pointer to Euler objects
 template<typename StateType, typename VolType, typename RandomType>
 using eulerPtr = shared_ptr<Euler<StateType, VolType, RandomType>>;
+
+/*------------------------------------------------------------------------------*/
+/*                              PHI SCHEME                                      */
+/*------------------------------------------------------------------------------*/
+
+template <typename StateType, typename VolType, typename RandomType>
+class PhiScheme
+{
+public:
+    /** Constructor. */
+    PhiScheme(const modelPtr<StateType, VolType, RandomType> model,
+              const schemePtr<StateType, VolType, RandomType> scheme,
+              std::function<StateType(StateType, StateType)> phi);
+
+    pair<StateType,StateType>  transition(double t, pair<StateType,StateType> x, double h, RandomType random);
+    pair<pair<StateType,StateType>,pair<StateType,StateType>> pairTransition(mt19937_64& gen, double t, pair<pair<StateType,StateType>,pair<StateType,StateType>>  x, double h, unsigned int n);
+
+    pair<StateType,StateType> singleSimulation(mt19937_64& gen, const unsigned int n);
+    pair<pair<StateType,StateType>,pair<StateType,StateType>> doubleSimulation(mt19937_64& gen, const unsigned int n1, const unsigned int n2);
+
+protected:
+    modelPtr<StateType, VolType, RandomType> m_model;
+    schemePtr<StateType, VolType, RandomType> m_scheme;
+    std::function<StateType(StateType, StateType)> m_phi;
+};
+
+/**
+ * @param model Pointer to the model
+ * @param scheme Pointer to the scheme
+ */
+template <typename StateType, typename VolType, typename RandomType>
+PhiScheme<StateType, VolType, RandomType>::PhiScheme(const modelPtr<StateType, VolType, RandomType> model,
+                                                     const schemePtr<StateType, VolType, RandomType> scheme,
+                                                     std::function<StateType(StateType, StateType)> phi):
+    Scheme<StateType, VolType, RandomType>(model), m_scheme(scheme), m_phi(phi){}
+
+
+/**
+ * @brief Method to make a transition of step h, at time t, starting from x and with a random realization random, applying at each step the functional \f$\phi\f$.
+ *
+ * @param t Starting time.
+ * @param x Starting point.
+ * @param h Time step.
+ * @param random Random realization.
+ * @return
+ */
+template <typename StateType, typename VolType, typename RandomType>
+pair<StateType,StateType>  PhiScheme<StateType, VolType, RandomType>::transition(double t, pair<StateType,StateType> x, double h, RandomType random)
+{
+    StateType S_t = x.first;
+    StateType phi_S_t = x.second;
+
+    S_t = m_scheme->transition(t, S_t, h, random);
+    phi_S_t = m_phi(S_t, phi_S_t);
+
+    return pair<StateType,StateType>(S_t, phi_S_t);
+}
+
+/**
+ * @brief Method to make a transition for two different discretizations of the same process, over step h, one with discretization 1 and one with discretization n,
+ * starting from x at time t, applying at each step the functional \f$\phi\f$.
+ *
+ * @param gen Random generator.
+ * @param t Starting time.
+ * @param x Starting point.
+ * @param h Time step.
+ * @param n Discretization for the finest process.
+ */
+template<typename StateType, typename VolType, typename RandomType>
+pair<pair<StateType,StateType>, pair<StateType,StateType>> PhiScheme<StateType, VolType, RandomType>::pairTransition(mt19937_64 &gen, double t,
+                                                                                                                     pair<pair<StateType,StateType>, pair<StateType,StateType>> pairProcess,
+                                                                                                                     double h,
+                                                                                                                     unsigned int n)
+{
+    // We get the finest step
+    double h_n = h/n;
+
+    // We get the starting points
+    pair<StateType,StateType> X_phiX_0 = pairProcess.first;
+    pair<StateType,StateType> X_phiX_n = pairProcess.second;
+
+    RandomType randomRealization;
+    RandomType randomRealizationSum;
+
+    // We make n transitions on the finest process, keeping memory of the random realizations
+    for (unsigned int i=0; i<n; ++i){
+        randomRealization = this->m_model->random(gen)*sqrt(h_n);
+        X_phiX_n = transition(t + i*h_n, X_phiX_n, h_n, randomRealization);
+        randomRealizationSum += randomRealization;
+    }
+
+    // We make one transition on the biggest process
+    X_phiX_0 = transition(t, X_phiX_0, h, randomRealizationSum);
+
+    return pair<pair<StateType,StateType>, pair<StateType,StateType>>(X_phiX_0, X_phiX_n);
+}
+
+/**
+ * @brief Method to simulate a realization of \f$X_T\f$.
+ *
+ * @param gen Generator.
+ * @param n Discretization over time.
+ * @return Simulated variable \f$X_T\f$.
+ */
+template<typename StateType, typename VolType, typename RandomType>
+pair<StateType, StateType> PhiScheme<StateType, VolType, RandomType>::singleSimulation(mt19937_64& gen, const unsigned int n)
+{
+    double currentTime = 0.;
+
+    StateType X_i = this->m_model->getStartingPoint();
+    StateType phi_X_i = m_phi(X_i, X_i);
+
+    pair<StateType,StateType> pair_i(X_i,phi_X_i);
+
+    double h = this->m_model->getMaturity()/(double)n;
+    for (unsigned int i=0; i<n; i++){
+        RandomType random = sqrt(h)*this->m_model->random(gen);
+        pair_i = transition(currentTime, pair_i, h, random);
+        currentTime += h;
+    }
+
+    return pair_i;
+}
+
+/**
+ * @brief Method to simulate two realizations of \f$(X_T, \phi(X_0, \ldots, X_T))\f$.
+ *
+ * The two discretizations used are \f$\frac{T-t_0}{n_1}\f$ and \f$\frac{T-t_0}{n_2}\f$.
+ *
+ * @param gen Generator.
+ * @param n1 Discretization over time for first simulation.
+ * @param n2 Discretization over time for second simulation.
+ * @return Pair of simulations of \f$(X_T, \phi(X_0, \ldots, X_T))\f$.
+ */
+template<typename StateType, typename VolType, typename RandomType>
+pair<pair<StateType,StateType>, pair<StateType,StateType>> PhiScheme<StateType, VolType, RandomType>::doubleSimulation(mt19937_64& gen, const unsigned int n1, const unsigned int n2)
+{
+    // We throw an error if n2 is not a multiple of n1
+    if (n2%n1 != 0){
+        cout << "Making a double simulation with two incompatible discretizations." << endl;
+        exit(1);
+    }
+
+    // We throw an error if n2 is not a bigger than n1
+    if (n2<n1){
+        cout << "Making a double simulation with two incompatible discretizations, it must be n1 < n2." << endl;
+        exit(1);
+    }
+
+    double currentTime = 0.;
+
+    unsigned int div = n2/n1;
+
+    StateType X_n1= this->m_model->getStartingPoint();
+    StateType X_n2 = this->m_model->getStartingPoint();
+
+    StateType phi_X_n1= m_phi(X_n1,X_n1);
+    StateType phi_X_n2 = m_phi(X_n2,X_n2);
+
+    pair<StateType,StateType> pair_n1(X_n1, phi_X_n1);
+    pair<StateType,StateType> pair_n2(X_n2, phi_X_n2);
+
+    double h1 = this->m_model->getMaturity()/(double)n1;
+
+    pair<pair<StateType,StateType>, pair<StateType,StateType>> pairProcess(pair_n1, pair_n2);
+
+    // We make a loop over the largest discretization and we make a pair transition over each step
+    for (unsigned int i=0; i<n1; ++i){
+        pairProcess = pairTransition(gen, currentTime, pairProcess, h1, div);
+        currentTime += h1;
+    }
+
+    return pairProcess;
+}
+
+// Smart pointer to Scheme objects
+template<typename StateType, typename VolType, typename RandomType>
+using phiSchemePtr = shared_ptr<PhiScheme<StateType, VolType, RandomType>> ;
 
 #endif // SCHEME_HPP
